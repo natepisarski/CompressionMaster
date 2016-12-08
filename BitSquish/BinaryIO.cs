0fc22e39910ebf06ca97e1d3405fa007a12b4fe2
Nathaniel Pisarski <nathanpisarski@gmail.com>
2016-12-08 13:05:39 -0500

    Initial Commit

---
+++ BitSquish/BinaryIO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BitSquish
+{
+	/// <summary>
+	/// Binary IO is responsible for writing and reading binary files.
+	/// </summary>
+	public class BinaryIO
+	{
+		public BinaryIO ()
+		{
+		}
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# CompressionMaster
+CompressionMaster is a suite of compression algorithms designed the shrink any file, binary or otherwise.
+
+# License
+MIT
